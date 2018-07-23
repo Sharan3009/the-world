6ad3fde0c72fc5bd27dcbe7bd2dd52fc54564f75
Sharan3009 <38356891+Sharan3009@users.noreply.github.com>
2018-07-23 21:15:48 +0530

    updating http errorresponse

--- src/app/restcountries.service.ts
+++ src/app/restcountries.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,13 +9,19 @@ export class RestcountriesService {
   private baseUrl:string = 'https://restcountries.eu/rest/v2'
   constructor(private http: HttpClient) { }
   
-  getAllCountries():Observable<any>{
+  public getAllCountries():Observable<any>{
     return this.http.get(`${this.baseUrl}/all?fields=region`)
   }
-  getAllCountriesWithFilter(filter:string,filterName:string):Observable<any>{
+  public getAllCountriesWithFilter(filter:string,filterName:string):Observable<any>{
     return this.http.get(`${this.baseUrl}/${filter}/${filterName}?fields=name;capital;flag;subregion;population;area`)
   }
-  getCountry(countryName:string):Observable<any>{
+  public getCountry(countryName:string):Observable<any>{
     return this.http.get(`${this.baseUrl}/name/${countryName}?fullText=true`)
+  }
+  // method to handle error
+  private handleError(err: HttpErrorResponse) {
+    console.log("Handle error http calls")
+    console.log(err.message);
+    return Observable.throw(err.message)
   }
 }
