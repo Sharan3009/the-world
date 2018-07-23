f24d683ca47a59f5b0c0afc65523c0dfca448328
sharan3009 <sharansingh3009@gmail.com>
2018-07-23 21:25:52 +0530

    updating comments

--- src/app/restcountries.service.ts
+++ src/app/restcountries.service.ts
@@ -8,13 +8,15 @@ import { Observable } from 'rxjs';
 export class RestcountriesService {
   private baseUrl:string = 'https://restcountries.eu/rest/v2'
   constructor(private http: HttpClient) { }
-  
+  // getting all countries to create region page
   public getAllCountries():Observable<any>{
     return this.http.get(`${this.baseUrl}/all?fields=region`)
   }
+  // getting all countries with filters applied
   public getAllCountriesWithFilter(filter:string,filterName:string):Observable<any>{
     return this.http.get(`${this.baseUrl}/${filter}/${filterName}?fields=name;capital;flag;subregion;population;area`)
   }
+  // getting single country
   public getCountry(countryName:string):Observable<any>{
     return this.http.get(`${this.baseUrl}/name/${countryName}?fullText=true`)
   }
