0913f7fb5ebfe4c56ac495a618ca285b6a5b664d
Sharan3009 <38356891+Sharan3009@users.noreply.github.com>
2018-07-25 23:07:52 +0530

    Adding interface

--- src/app/shared/names/names.component.ts
+++ src/app/shared/names/names.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Names } from './names';
 
 @Component({
   selector: 'app-names',
   templateUrl: './names.component.html',
   styleUrls: ['./names.component.css']
 })
-export class NamesComponent implements OnInit {
+export class NamesComponent implements OnInit, Names {
   // taking input from the parent component
   public show: number = 0; // to display translations on the click of show more
   @Input() name: any;
